e1b75fc8e46c47a7876e5bdd15b24580b43d9427
Sune Selchow <techdragonhq@gmail.com>
2021-10-05 11:37:34 +0200

    Basic fixes to make runnable

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,6 +32,7 @@ def create_app(test_config=None):
 
     from . import blog
     app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     
     from . import booking
     app.register_blueprint(booking.bp)
