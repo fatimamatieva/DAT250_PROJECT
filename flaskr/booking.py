4519939a7bbf476e2165e87cd888ed44e1d5a9a7
Elisa Gunnarshaug <e.gunnarshaug@stud.uis.no>
2021-10-13 10:32:23 +0200

    new booking functionality

--- flaskr/booking.py
+++ flaskr/booking.py
@@ -1,5 +1,6 @@
+from sqlite3.dbapi2 import Date
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify, make_response
 )
 from werkzeug.exceptions import abort
 
@@ -11,6 +12,7 @@ from datetime import datetime
 bp = Blueprint('booking', __name__)
 
 @bp.route('/booking', methods=('GET', 'POST'))
+#@login_required
 def booking():
     rooms = []
     if request.method == 'POST':
@@ -19,37 +21,57 @@ def booking():
         rooms = get_rooms(date, time)
     return render_template('booking/booking.html', rooms=rooms)
 
-@bp.route('/booking/confirm/', methods=('GET', 'POST'))
+@bp.route("/booking/confirm", methods = ['POST'])
 #@login_required
 def confirm():
-    if request.method == 'POST':
-        room = request.form['room']
-        date = request.form['date']
-        time = request.form['time']
-        comments = request.form['comments']
-        db = get_db()
-        db.execute(
-            'INSERT INTO room_time (room_id, from_time, to_time, Comments, user_id)'
-            ' VALUES (?, ?, ?, ?, ?)',
-            (date, room, time, comments, g.user['id'])
-        )
-        db.commit()
-        flash("Booking successful")
-        return redirect(url_for('blog.index'))
-    return render_template('booking/confirm.html')
+    if request.is_json:
+        data = request.get_json()
+        print(data)
+        room = data['room_number']
+        #date = data['date']
+        #time = data['time']
+        #TODO:  insert new booking into database
+        #       should the user have a limited amount of reservations?
 
-def get_room(date, time, room):
+        response = {'message': 'Booking confirmed', 'code': 'SUCCESS'}
+        flash(f'Booking for room {room} confirmed')
+        return make_response(jsonify(response), 201)
+    response = {'message': 'Something want wrong', 'code': 'ERROR'}
+
+    return make_response(jsonify(response),400)
+
+#@bp.route('/booking/confirm/', methods=('GET', 'POST'))
+#@login_required
+#def confirm():
+#    if request.method == 'POST':
+#        room = request.form['room']
+#        date = request.form['date']
+#        time = request.form['time']
+#        comments = request.form['comments']
+#        db = get_db()
+#        db.execute(
+#            'INSERT INTO room_time (room_id, from_time, to_time, Comments, user_id)'
+#            ' VALUES (?, ?, ?, ?, ?)',
+#            (date, room, time, comments, g.user['id'])
+#        )
+#        db.commit()
+#        flash("Booking successful")
+#        return redirect(url_for('blog.index'))
+#
+#    return render_template('booking/confirm.html')
+
+# def get_room(date, time, room):
     
-    room = get_db().execute(
-        'SELECT RoomName, Date, Time'
-        ' FROM rooms'
-        ' WHERE Date = ? AND Time = ? AND RoomName= ?',
-        (date,time,room,)
-    ).fetchone()
+#     room = get_db().execute(
+#         'SELECT RoomName, Date, Time'
+#         ' FROM rooms'
+#         ' WHERE Date = ? AND Time = ? AND RoomName= ?',
+#         (date,time,room,)
+#     ).fetchone()
 
-    if room is None:
-        abort(404)
-    return room  
+#     if room is None:
+#         abort(404)
+#     return room  
 
 def get_rooms(date, time):
 #    rooms = get_db().execute(
--- flaskr/static/booking.js
+++ flaskr/static/booking.js
@@ -1,21 +1,69 @@
 
- 
-window.onload = function() {
-    var dateControl = document.querySelector('input[type="date"]');
+
+$(function() {
+    //setting default value, and minimum date. 
+    var dateControl = $('input[type="date"]');
     var dateFormat = getDateFormat(new Date());
+    dateControl.val(dateFormat);
+    dateControl.attr('min',dateFormat);
+});
   
-    dateControl.value = dateFormat;
-    dateControl.min = dateFormat;
-  };
-  
-  function getDateFormat(date){
-    var day = date.getDate(),
-       month = date.getMonth() + 1;
-    if (day.toFixed().length == 1){
-      day = '0' + day;
-    }
-    if (month.toFixed().length == 1){
-      month = '0' + month;
-    }
-    return date.getFullYear() + '-' + month + '-' + day
+function getDateFormat(date){
+  var day = date.getDate(),
+      month = date.getMonth() + 1;
+  if (day.toFixed().length == 1){
+    day = '0' + day;
   }
+  if (month.toFixed().length == 1){
+    month = '0' + month;
+  }
+  return date.getFullYear() + '-' + month + '-' + day
+}
+
+function Confirm(room){
+
+  var confirmbox = $("#confirmBox");
+  var span = $(".close")[0];
+
+// when the user clicks anywhere outside of the modal or clicks on the close btn, close it
+  span.onclick = function() {
+    closeConfirmBox(confirmbox);
+  }
+  window.onclick = function(event) {
+    if (event.target == confirmbox[0]) {
+      closeConfirmBox(confirmbox[0]);
+    }
+  }
+
+  confirmbox.show();
+  $(".confirm-text>span").html(room)
+
+  $("#btnConfirm").on("click", function(ev){
+    ev.preventDefault();
+    var reservation = {
+      date: new Date($("#selectedDate").val()),
+      time: $("#selectedTime").val(),
+      room_number: room,
+    }
+    $.ajax({
+      type: "POST",
+      url: "/booking/confirm",
+      data: JSON.stringify(reservation),
+      contentType: "application/json",
+      dataType: 'json',
+      success: function(){
+        //redirect to homepage if booking is success
+        location.href = '/';
+      },
+      error: function(){
+        closeConfirmBox(confirmbox[0]);
+      }
+    });
+  });
+}
+
+function closeConfirmBox(elem){
+  //hide confirmbox and turn off event-listner
+  $(elem).hide();
+  $(elem).find("#btnConfirm").off("click");
+}
