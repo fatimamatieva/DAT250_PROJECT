1d436a2e9cc9e83981866236ab5f9510864a5b78
Elisa Gunnarshaug <e.gunnarshaug@stud.uis.no>
2021-10-19 12:29:50 +0200

    wtf-forms

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,9 +9,39 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-
+from .forms import *
 
 @bp.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+        
+
+        flash(error)
+
+    return render_template('auth/register.html', form=form)
+
 # def register():
 #     if request.method == 'POST':
 #         username = request.form['username']
@@ -41,19 +71,41 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 #     return render_template('auth/register.html')
 
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
-        db.session.add(user)
-        db.session.commit()
-        flash(f'Your account has been created! You can now log in.', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+# def register():
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash(f'Your account has been created! You can now log in.', 'success')
+#         return redirect(url_for('home'))
+#     return render_template('register.html', title='Register', form=form)
 
 
 @bp.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None or not check_password_hash(user['password'], password):
+            error = 'Incorrect username or password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html', form=form)
 # def login():
 #     if request.method == 'POST':
 #         username = request.form['username']
@@ -77,15 +129,15 @@ def register():
 #         flash(error)
 
 #     return render_template('auth/login.html')
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash(f'You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+#             flash(f'You have been logged in!', 'success')
+#             return redirect(url_for('home'))
+#         else:
+#             flash('Login Unsuccessful. Please check username and password', 'danger')
+#     return render_template('login.html', title='Login', form=form)
 
 @bp.route('/profile', methods=('GET', 'POST'))
 def profile():
--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,42 +1,52 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskr import db
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField, BooleanField
+# from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+# from flaskr import db
+from wtforms import Form, StringField, PasswordField,SubmitField, BooleanField, validators
 
 
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+#     password = db.Column(db.String(60), nullable=False)
+#     posts = db.relationship('Post', backref='author', lazy=True)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+#     def __repr__(self):
+#         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
         
-class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)]) # Validators to provide a basic requirement for the username
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Retype Password', validators=[DataRequired(), EqualTo('password')])
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('Retype Password', message='Passwords must match')
+    ])
+    confirm_password = PasswordField('Confirm Password')
     submit = SubmitField('Sign Up')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is already taken! Please try another username.')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email already exists.')
+    # username = StringField('Username',
+    #                        validators=[DataRequired(), Length(min=2, max=20)]) # Validators to provide a basic requirement for the username
+    # email = StringField('Email', validators=[DataRequired(), Email()])
+    # password = PasswordField('Password', validators=[DataRequired()])
+    # confirm_password = PasswordField('Retype Password', validators=[DataRequired(), EqualTo('password')])
+    # submit = SubmitField('Sign Up')
+
+#     def validate_username(self, username):
+#         user = User.query.filter_by(username=username.data).first()
+#         if user:
+#             raise ValidationError('That username is already taken! Please try another username.')
+
+#     def validate_email(self, email):
+#         user = User.query.filter_by(email=email.data).first()
+#         if user:
+#             raise ValidationError('That email already exists.')
 
 
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+class LoginForm(Form):
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', [validators.DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
