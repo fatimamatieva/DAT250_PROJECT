59fb142c42b013868c0d45f27de08e4c0827abda
Elisa Gunnarshaug <e.gunnarshaug@stud.uis.no>
2021-10-20 16:14:06 +0200

    forms

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -2,9 +2,9 @@
 # from wtforms import StringField, PasswordField, SubmitField, BooleanField
 # from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 # from flaskr import db
-from wtforms import Form, StringField, PasswordField,SubmitField, BooleanField, validators
-from wtforms.validators import InputRequired, Email
-
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField,SubmitField, BooleanField, validators
+from flaskr.db import get_db
 
 # class User(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -17,16 +17,27 @@ from wtforms.validators import InputRequired, Email
 #     def __repr__(self):
 #         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
         
-class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
+class RegistrationForm(FlaskForm):
     email = StringField('Email', [validators.DataRequired(), validators.Email()])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm_password', message='Passwords must match')
+        #TODO:
+        #password policy? symbols/uppercase/lowercase
+        # validators.Regexp(pattern)
     ])
     confirm_password = PasswordField('Confirm Password')
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('Resgister')
 
+    def validate_email(self, email):
+        print(email.data)
+        db = get_db()
+
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (email.data,)
+        ).fetchone()
+        if user:
+            raise validators.ValidationError('Email is already registered.')
 
     # username = StringField('Username',
     #                        validators=[DataRequired(), Length(min=2, max=20)]) # Validators to provide a basic requirement for the username
@@ -46,7 +57,7 @@ class RegistrationForm(Form):
 #             raise ValidationError('That email already exists.')
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email', [validators.DataRequired(), validators.Email()])
     password = PasswordField('Password', [validators.DataRequired()])
     remember = BooleanField('Remember Me')
