3deca8293ad70347b86b80618a0816b6bc435431
Sune Selchow <techdragonhq@gmail.com>
2021-10-22 12:41:28 +0200

    server autorun added

---
+++ Win11AutoRun&Log - Server.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path log.txt -append
+
+while (1) {
+venv\Scripts\activate
+$env:FLASK_APP = "flaskr"
+$env:FLASK_ENV = "development"
+py -m flask init-db
+py -m flask run --host=0.0.0.0
+}
+
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue" # or "Stop"
