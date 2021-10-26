6dcdbef7d12a6616db9fb59a19cf207cb6b2cc06
Fatima Matieva <fatima8482@hotmail.com>
2021-10-26 10:18:56 +0200

    --

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -6,28 +6,37 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField, BooleanField, validators
 from flaskr.db import get_db
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-#     password = db.Column(db.String(60), nullable=False)
-#     posts = db.relationship('Post', backref='author', lazy=True)
-
-#     def __repr__(self):
-#         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
         
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    email = StringField('Email', [validators.DataRequired(), validators.Email(), validators.Length(min=8, max=20)])
     password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm_password', message='Passwords must match')
+        validators.EqualTo('Confirm_password', message='Passwords must match')
         #TODO:
         #password policy? symbols/uppercase/lowercase
         # validators.Regexp(pattern)
+    
     ])
     confirm_password = PasswordField('Confirm Password')
     submit = SubmitField('Register')
+
+    def validate_password(self, password):
+        upper_count = 0
+        letter_count = 0
+        numb_count = 0
+
+        for i in password.data:
+            if i.isalpha():
+                letter_count += 1
+            if i.isdigit():
+                numb_count += 1
+            if i.isupper():
+                upper_count += 1
+
+        if numb_count > 0 and upper_count > 0:
+            return True
+        else:
+            raise ValueError('The password must have at least one number and one upper case letter')
 
     def validate_email(self, email):
         print(email.data)
@@ -39,26 +48,43 @@ class RegistrationForm(FlaskForm):
         if user:
             raise validators.ValidationError('Email is already registered.')
 
-    # username = StringField('Username',
-    #                        validators=[DataRequired(), Length(min=2, max=20)]) # Validators to provide a basic requirement for the username
-    # email = StringField('Email', validators=[DataRequired(), Email()])
-    # password = PasswordField('Password', validators=[DataRequired()])
-    # confirm_password = PasswordField('Retype Password', validators=[DataRequired(), EqualTo('password')])
-    # submit = SubmitField('Sign Up')
-
-#     def validate_username(self, username):
-#         user = User.query.filter_by(username=username.data).first()
-#         if user:
-#             raise ValidationError('That username is already taken! Please try another username.')
-
-#     def validate_email(self, email):
-#         user = User.query.filter_by(email=email.data).first()
-#         if user:
-#             raise ValidationError('That email already exists.')
-
 
 class LoginForm(FlaskForm):
     email = StringField('Email', [validators.DataRequired(), validators.Email()])
     password = PasswordField('Password', [validators.DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class ChangePassword(FlaskForm):
+    email = StringField('Email', [validators.DataRequired(), validators.Email(), validators.Length(min=8, max=20)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('Confirm_password', message='Passwords must match')
+        #TODO:
+        #password policy? symbols/uppercase/lowercase
+        # validators.Regexp(pattern)
+    
+    ])
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Change Password')
+
+    def validate_password(self, password):
+        upper_count = 0
+        letter_count = 0
+        numb_count = 0
+
+        for i in password.data:
+            if i.isalpha():
+                letter_count += 1
+            if i.isdigit():
+                numb_count += 1
+            if i.isupper():
+                upper_count += 1
+
+        if numb_count > 0 and upper_count > 0:
+            return True
+        else:
+            raise ValueError('The password must have at least one number and one upper case letter')
+
+            
