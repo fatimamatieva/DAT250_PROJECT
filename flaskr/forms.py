53fd9a364bce91861eededb37010d5255c8712f0
Elisa Gunnarshaug <e.gunnarshaug@stud.uis.no>
2021-10-28 11:46:23 +0200

    password policy

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,9 +1,5 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, PasswordField, SubmitField, BooleanField
-# from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-# from flaskr import db
 from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField,SubmitField, BooleanField, validators
+from wtforms import StringField, PasswordField,SubmitField, validators
 from flaskr.db import get_db
 
         
@@ -17,36 +13,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', [
         validators.DataRequired(message="Password is required"),
         validators.EqualTo('confirm_password', message="Passwords must match"),
-        #TODO:
-        #PASSWORD POLICY
-        # validators.Regexp(
-        #     "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-        #     message="Password must have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
-        # )
+        validators.Regexp(
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$",
+            message="Password must have minimum eight characters, at least one uppercase letter, one lowercase letter and one number"
+        )
 
     ])
     confirm_password = PasswordField('Confirm Password',[
         validators.DataRequired(message="Confirm password is required")
     ])
     submit = SubmitField('Register')
-
-    def validate_password(self, password):
-        upper_count = 0
-        letter_count = 0
-        numb_count = 0
-
-        for i in password.data:
-            if i.isalpha():
-                letter_count += 1
-            if i.isdigit():
-                numb_count += 1
-            if i.isupper():
-                upper_count += 1
-
-        if numb_count > 0 and upper_count > 0:
-            return True
-        else:
-            raise ValueError('The password must have at least one number and one upper case letter')
 
     def validate_email(self, email):
         db = get_db()
@@ -66,7 +42,6 @@ class LoginForm(FlaskForm):
     ])
 
     password = PasswordField('Password', [validators.DataRequired(message="Password is required")])
-    # remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 
@@ -77,32 +52,13 @@ class ChangePassword(FlaskForm):
     new_password = PasswordField('New Password', [
         validators.DataRequired(message="New Password is required"),
         validators.EqualTo('confirm_new_password', message="Passwords must match"),
-        #PASSWORD POLICY
-        # validators.Regexp(
-        #     "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-        #     message="Password must have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
-        # )
+        validators.Regexp(
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$",
+            message="Password must have minimum eight characters, at least one uppercase letter, one lowercase letter and one number"
+        )
     
     ])
     confirm_new_password = PasswordField('Confirm New Password',[
         validators.DataRequired(message="Confirm new password is required")
     ])
     submit = SubmitField('Change Password')
-
-    def validate_new_password(self, new_password):
-        upper_count = 0
-        letter_count = 0
-        numb_count = 0
-
-        for i in new_password.data:
-            if i.isalpha():
-                letter_count += 1
-            if i.isdigit():
-                numb_count += 1
-            if i.isupper():
-                upper_count += 1
-
-        if numb_count > 0 and upper_count > 0:
-            return True
-        else:
-            raise ValueError('The password must have at least one number and one upper case letter')
