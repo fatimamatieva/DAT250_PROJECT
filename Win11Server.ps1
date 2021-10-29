ffa2de599b3a9456be050e56b38cb67e0968c8e6
Sune Selchow <techdragonhq@gmail.com>
2021-10-29 05:03:23 +0200

    ADDED SSL! fix

--- Win11Server.ps1
+++ Win11Server.ps1
@@ -5,7 +5,7 @@ Start-Transcript -path log.txt -append
 
 while (1) {
 venv\Scripts\activate
-py -m waitress 'wsgi:app'
+py -m waitress --listen=*:5000 --url-scheme=https 'wsgi:app'
 }
 
 $ErrorActionPreference="SilentlyContinue"
